cd56435b96081006f5b32fbbc54e9d6a9657fb16
wujiyu115 <wjy151486@alibaba-inc.com>
2024-07-09 19:15:00 +0800

    update docker

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -4,9 +4,9 @@ run () {
   echo "Connecting to openconnect..."
   echo $OPENCONNECT_PASSWORD | openconnect -u "$OPENCONNECT_USER" $OPENCONNECT_OPTIONS --passwd-on-stdin $OPENCONNECT_URL
   echo "Connected to openconnect!"
-
-  squid3 -NYCd 1
 }
+
+squid3 -NYCd 1
 
 until (run); do
   echo "openconnect exited. Restarting process in 60 seconds…" >&2
