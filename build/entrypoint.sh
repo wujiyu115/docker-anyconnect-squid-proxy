dd71f132c39afceec0ca7b01e8b196d6693d78af
wujiyu115 <wjy151486@alibaba-inc.com>
2024-07-09 19:07:56 +0800

    update dockerfile

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -2,18 +2,7 @@
 
 run () {
   echo "Connecting to openconnect..."
-  # Start openconnect
-  if [ -z "${OPENCONNECT_PASSWORD}" ]; then
-    # Ask for password
-    openconnect -u "$OPENCONNECT_USER" $OPENCONNECT_OPTIONS $OPENCONNECT_URL
-  elif [ ! -z "${OPENCONNECT_PASSWORD}" ] && [ ! -z "${OPENCONNECT_MFA_CODE}" ]; then
-    # Multi factor authentication (MFA)
-    (echo $OPENCONNECT_PASSWORD; echo $OPENCONNECT_MFA_CODE) | openconnect -u "$OPENCONNECT_USER" $OPENCONNECT_OPTIONS --passwd-on-stdin $OPENCONNECT_URL
-  elif [ ! -z "${OPENCONNECT_PASSWORD}" ]; then
-  # Standard authentication
-    echo $OPENCONNECT_PASSWORD | openconnect -u "$OPENCONNECT_USER" $OPENCONNECT_OPTIONS --passwd-on-stdin $OPENCONNECT_URL
-  fi
-
+  echo $OPENCONNECT_PASSWORD | openconnect -u "$OPENCONNECT_USER" $OPENCONNECT_OPTIONS --passwd-on-stdin $OPENCONNECT_URL
   echo "Connected to openconnect!"
 
   squid3 -NYCd 1
